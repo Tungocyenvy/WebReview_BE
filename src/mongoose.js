18c0a3aa542e61be3abd8a1b0d065ff4a1788663
Nguyen Thi Nhu Quynh <67548525+quynhbud@users.noreply.github.com>
2021-11-05 22:16:42 +0700

    delete mongosee

--- src/mongoose.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  multipleMongooseToObject: function (mongooses) {
-    return mongooses.map((mongooses) => mongooses.toObject());
-  },
-
-  MongooseToObject: function (mongoose) {
-    return mongoose ? mongoose.toObject() : mongoose;
-  },
-};
